1a4eaaef37ee7ea77a8f6d04f6cf39c4bc5154f9
DanaOHara <rosa.munozv@usach.cl>
2017-11-29 17:37:59 -0300

    acceso a la base de datos ok

--- indexp2.php
+++ indexp2.php
@@ -1,28 +1,59 @@
 <?php
 session_start();
 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "moodleMABP";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname);
+
+
+if ($conn->connect_error)
+{
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id FROM mdl_user WHERE username = 'admin'";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Usuario: " . $row["username"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+
 function redirigirVacio()
 {
     header("Location: vacio.html");
 
 }
 
-function redirigirCursos($pass)
+function redirigirSegundaParte($pass)
 {
-  $_SESSION['pass'] = $pass;
-  header("Location: cursosInicio.html");
+    $_SESSION['pass'] = $pass;
+    header("Location: cursosInicio.html");
 
 }
 
-if($_POST['pass'] = '')
+
+if($_POST['pass'] == '')
 {
   redirigirVacio();
 }
 
 else
 {
-redirigirCursos($POST['pass']);
+  redirigirSegundaParte($_POST['pass'] );
 }
+
 
 
 ?>
